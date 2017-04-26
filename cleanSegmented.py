9b2664d91db0021b3c036da2741574f60d0213a5
arivanbuuren <ariadna.estrada42@gmail.com>
2017-04-26 14:20:36 -0700

    Updated code to assign class to each cluster
    1. Select class to assign according to list
    2. Click on a desired region to assign class
    3. Press "c" to close

--- cleanSegmented.py
+++ cleanSegmented.py
@@ -4,71 +4,68 @@ import numpy as np
 IM_WIDTH = 160
 IM_HEIGHT = 120
 
+unassigned = [255, 255, 255]
 grass = [0, 200, 0]
 road = [100, 100, 100]
-
 sidewalk = [0, 255, 255]
 dirt = [45,80,160]
 bushes = [32,160,220]
-rd = road 
-bs = bushes
-sdw = sidewalk
-drt = dirt
+obstacle = [0, 0, 0] 
 
-imgNumber = 2
-imageName = 'segmentedImage/image'+ str(imgNumber)+ '.ppm'
+
+imgNumber = 1
+imageName = 'clusters/image'+ str(imgNumber)+ '.ppm'
 saveName = 'cleanSegments/image'+ str(imgNumber)+ '.ppm'
 
-img = cv2.imread(imageName)
-img2 = img
+currClass = 1
+myClasses = {'unassigned':0, 'grass':1, 'road':2, 'sidewalk':3, 'dirt':4, 'bushes':5, 'obstacle':6}
+myClassesList = [unassigned, grass, road, sidewalk, dirt, bushes, obstacle]
 
-segments = np.unique(img[:,:,0], return_counts = True)
-indicesA = np.where(img[:,:,0] == 2);A = sdw
-indicesB = np.where(img[:,:,0] == 14);B = grass
-indicesC = np.where(img[:,:,0] == 55);  C = rd
-indicesD = np.where(img[:,:,0] == 77); D = grass
-indicesE = np.where(img[:,:,0] == 135); E = rd
-indicesF = np.where(img[:,:,0] == 151);F = rd
-indicesG = np.where(img[:,:,0] == 206);G = rd
-indicesH = np.where(img[:,:,0] == 248);H = drt
-indicesI = np.where(img[:,:,0] == 249);I = grass
-indicesJ = np.where(img[:,:,0] == 252);J = bs
-indicesK = np.where(img[:,:,0] == 75);K = bs
-   
-for i in range(len(indicesA[0])):
-    img2[indicesA[0][i], indicesA[1][i], :] = A
-    
-    
-for i in range(len(indicesB[0])):
-    img2[indicesB[0][i], indicesB[1][i], :] = B
-    
-for i in range(len(indicesC[0])):
-    img2[indicesC[0][i], indicesC[1][i], :] = C
-    
-for i in range(len(indicesD[0])):
-    img2[indicesD[0][i], indicesD[1][i], :] = D
-    
-for i in range(len(indicesE[0])):
-    img2[indicesE[0][i], indicesE[1][i], :] = E
+def onMouseClick(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+        assign_class(x,y, param)
 
-for i in range(len(indicesF[0])):
-    img2[indicesF[0][i], indicesF[1][i], :] = F
-
-for i in range(len(indicesG[0])):
-    img2[indicesG[0][i], indicesG[1][i], :] = G
-
-for i in range(len(indicesH[0])):
-    img2[indicesH[0][i], indicesH[1][i], :] = H
-for i in range(len(indicesI[0])):
-    img2[indicesI[0][i], indicesI[1][i], :] = I
+def assign_class(x,y, image):
+    strClass = (list(myClasses.keys())[list(myClasses.values()).index(currClass)])
     
-for i in range(len(indicesJ[0])):
-    img2[indicesJ[0][i], indicesJ[1][i], :] = J
-    
-for i in range(len(indicesK[0])):
-    img2[indicesK[0][i], indicesK[1][i], :] = K
+    print "You are currently assigning: ", strClass
+    clusterColor =  image[y,x,:]
+    clusterIndices = np.where(image[:,:,0] == clusterColor[0])
 
-cv2.imshow("segmented", img2)
+    for i in range(len(clusterIndices[0])):
+        image[clusterIndices[0][i], clusterIndices[1][i], :] = myClassesList[currClass]
+       
+    cv2.imwrite(imageName, image)
 
 
-cv2.imwrite(saveName, img2)
+image = cv2.imread(imageName)
+cv2.namedWindow("clusters")
+cv2.setMouseCallback("clusters", onMouseClick, param = image)
+
+while True:
+    cv2.imshow("clusters", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("c"):
+        break
+    elif key == ord("1"):
+        print "Changed to Grass"
+        currClass = 1
+    elif key == ord("2"):
+        print "Changed to Road"
+        currClass = 2
+    elif key == ord("3"):
+        print "Changed to Sidewalk"
+        currClass = 3
+    elif key == ord("4"):
+        print "Changed to Dirt"
+        currClass = 4
+    elif key == ord("5"):
+        print "Changed to Bushes"
+        currClass = 5
+    elif key == ord("6"):
+        print "Changed to Obstacle"
+        currClass = 6
+        
+cv2.destroyAllWindows()
+
