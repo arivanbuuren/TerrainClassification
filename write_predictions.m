fceae8b632ec5093b89456eba1fd516fff60f726
arivanbuuren <ariadna.estrada42@gmail.com>
2017-04-29 12:49:20 -0700

    Predictions made with NNet -- 100 iterations during training

--- write_predictions.m
+++ write_predictions.m
@@ -1,6 +1,6 @@
 clear;
-load('yhat.mat');
-% load('prediction.mat');
+% load('yhat.mat');
+load('NeuralNet code/prediction.mat');
 im_w = 120;
 im_h = 160;
 numImages =  length(yhat) / (im_w * im_h) ; 
@@ -8,6 +8,6 @@ predictions = reshape(yhat, [im_w*im_h,numImages]);
 
 for im = 1:numImages
    yvis = visualizePrediction(predictions(:,im));
-   imName = char(strcat('classifiedImages/image', string(im), '.png' ));
+   imName = char(strcat('classifiedImages/NNet/image', string(im), '.png' ));
    imwrite(yvis, imName);
 end
