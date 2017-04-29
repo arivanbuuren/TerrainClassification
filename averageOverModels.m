d14ed5961b617e6839d18120377fdc2d95de5200
CandiceT93 <candicetian93@gmail.com>
2017-04-29 16:20:26 -0700

    decision tree

--- averageOverModels.m
+++ averageOverModels.m
@@ -1,16 +1,16 @@
-load 'prediction.mat';
+load 'NeuralNet code/prediction.mat';
 yhat_CNN = yhat;
 
 load 'predictionGMM.mat';
 yhat_GMM = yhat;
 
-load 'predictionDecisionTree.mat'
+load 'a1/predictionDecisionTree.mat'
 yhat_DT = yhat;
 
 load 'testingData.mat'
 
 Yhat = [yhat_CNN yhat_GMM yhat_DT];
 yhat = mode(Yhat, 2);
-accuracy = mean(yhat ~= ytest);
+accuracy = mean(yhat == ytest);
 
 fprintf('Accuracy of averaging is %.2f\n', accuracy);
