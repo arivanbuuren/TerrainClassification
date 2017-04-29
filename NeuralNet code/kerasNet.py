ee751e7a6f8b56331ac4429facaa950f05e61d7b
arivanbuuren <ariadna.estrada42@gmail.com>
2017-04-29 11:15:33 -0700

    NNet code and prediction updated with 7 classes

--- NeuralNet code/kerasNet.py
+++ NeuralNet code/kerasNet.py
@@ -2,14 +2,13 @@ from keras.utils import np_utils, to_categorical
 from keras.models import Sequential, load_model
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
-import processImages 
 import numpy as np
 import scipy.io
 
 class kerasNet:
     def __init__(self):
-        self.trainingData = scipy.io.loadmat('trainingData.mat')
-        self.validationData = scipy.io.loadmat('testingData.mat')
+        self.trainingData = scipy.io.loadmat('../trainingData.mat')
+        self.validationData = scipy.io.loadmat('../testingData.mat')
 
     def train(self):
         print "Training neural net keras"
@@ -18,23 +17,33 @@ class kerasNet:
         xTrain = self.trainingData['Xtrain']
         yTrain = self.trainingData['ytrain']
 
+        for i in range(len(yTrain)):
+            if yTrain[i] == 8: 
+                yTrain[i] = 0
+
         xValid = self.validationData['Xtest']
         yValid = self.validationData['ytest']
 
-        #Setup network architecture
+        for i in range(len(yValid)):
+            if yValid[i] == 8: 
+                yValid[i] = 0
+
+        nDimensions = xTrain.shape[1]
+        
+        # #Setup network architecture
         self.model = Sequential()
-        self.model.add(Dense(64, input_shape=(3,), activation='relu', use_bias=True))
+        self.model.add(Dense(64, input_shape=(nDimensions,), activation='relu', use_bias=True))
         self.model.add(Dropout(0.5))
         self.model.add(Dense(64, activation='relu'))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(6, activation='softmax'))
+        self.model.add(Dense(8, activation='softmax'))
         self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-        one_hot_labels = to_categorical(yTrain, num_classes=6)
-        self.model.fit(xTrain, one_hot_labels, epochs=100, batch_size=128)
+        one_hot_labels = to_categorical(yTrain, num_classes=8)
+        self.model.fit(xTrain, one_hot_labels, epochs=10, batch_size=128)
 
-        one_hot_validation = to_categorical(yValid, num_classes=6)
+        one_hot_validation = to_categorical(yValid, num_classes=8)
         score = self.model.evaluate(xValid, one_hot_validation, verbose=0)
         print('Test accuracy:', score[1])
         self.model.save('myKerasNet.h5')
