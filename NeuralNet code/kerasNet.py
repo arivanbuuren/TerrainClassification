cacc83c88ee05ee368cb99c92c446a96ff2894f5
arivanbuuren <ariadna.estrada42@gmail.com>
2017-04-29 13:03:21 -0700

    NNet batch size = 1 image

--- NeuralNet code/kerasNet.py
+++ NeuralNet code/kerasNet.py
@@ -41,7 +41,7 @@ class kerasNet:
 
         #Fit model
         one_hot_labels = to_categorical(yTrain, num_classes=8)
-        self.model.fit(xTrain, one_hot_labels, epochs=100, batch_size=128)
+        self.model.fit(xTrain, one_hot_labels, epochs=10, batch_size=120*160)
 
         #Save model
         self.model.save('myKerasNet.h5')
