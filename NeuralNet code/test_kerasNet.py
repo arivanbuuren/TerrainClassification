d83f63aee4835cc8e90b9f0706256ae9225e2bb4
arivanbuuren <ariadna.estrada42@gmail.com>
2017-04-29 17:11:04 -0700

    all matrices

--- NeuralNet code/test_kerasNet.py
+++ NeuralNet code/test_kerasNet.py
@@ -7,14 +7,26 @@ from keras.models import load_model
 if __name__ == "__main__":
     #Load test data
     testData = scipy.io.loadmat('../testingData.mat')
+    trainingData = scipy.io.loadmat('../trainingData.mat')
+
+    xTrain = trainingData['Xtrain']
+    yTrain = trainingData['ytrain']
+
     xValid = testData['Xtest']
     yValid = testData['ytest']
 
     #Load model
     model = load_model('myKerasNet.h5')
-    pyx = model.predict(xValid)
 
-    yhat = np.argmax(pyx, axis=1)
-    yhat = np.reshape(yhat, (yhat.shape[0],1))
-    print yhat.shape
-    scipy.io.savemat('prediction.mat', {'yhat' : yhat, 'pyx' : pyx[:,1:8]})
+    #Predictions on training data
+    pyx_train = model.predict(xTrain)
+    yhat_train = np.argmax(pyx_train, axis=1)
+    yhat_train = np.reshape(yhat_train, (yhat_train.shape[0],1))
+
+    #Predictions on test data
+    pyx_test = model.predict(xValid)
+    yhat_test = np.argmax(pyx_test, axis=1)
+    yhat_test = np.reshape(yhat_test, (yhat_test.shape[0],1))
+
+    #Save probabilities and predictions
+    scipy.io.savemat('prediction.mat', {'yhat_train' : yhat_train, 'pyx_train' : pyx_train[:,1:8], 'yhat_test' : yhat_test, 'pyx_test' : pyx_test[:,1:8]})
