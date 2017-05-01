66929215b5f4967d824de7b4cd406f8ff79bbe87
arivanbuuren <ariadna.estrada42@gmail.com>
2017-05-01 13:57:45 -0700

    moved GMM code to its own folder

---
+++ GMMcode/testGMM.m
@@ -0,0 +1,25 @@
+%This program fits a Gaussian mixture model to make pixel-wise
+%classification
+
+close all;
+clear;clc;
+
+%Load data
+load '../BestDataset/trainingData.mat'
+load '../BestDataset/testingData.mat'
+% Xtrain = Xtrain(:,1:3);
+% Xtest = Xtest(:,1:3);
+% Fit GMM on train data and compute accuracy 
+
+model = generativeGaussian(Xtrain, ytrain);
+[yhat_train_GMM, pyx_train_GMM] = model.predict(model,Xtrain);
+fprintf('Gaussian Gen. Model. training accuracy is %.2f\n', mean(yhat_train_GMM == ytrain));
+
+% Make predictions on test data
+[yhat_test_GMM, pyx_test_GMM] = model.predict(model, Xtest);
+fprintf('Gaussian Gen. Model. testing accuracy is %.2f\n', mean(yhat_test_GMM == ytest));
+
+%Save predictions and probabilities on training data
+pyx_train_GMM = pyx_train_GMM(:,1:7);
+pyx_test_GMM = pyx_test_GMM(:,1:7);
+save('predictionGMMwith.mat', 'yhat_train_GMM', 'pyx_train_GMM', 'yhat_test_GMM', 'pyx_test_GMM');
