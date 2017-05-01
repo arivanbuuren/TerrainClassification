5fde7bd4cbb46cdabac02884cb388cf807bf990d
arivanbuuren <ariadna.estrada42@gmail.com>
2017-05-01 14:58:58 -0700

    Random Forest results

--- RandomForest/decisionForest.m
+++ RandomForest/decisionForest.m
@@ -2,12 +2,14 @@ function [model] = decisionForest(X,y,depth,nBootstraps)
 [n,d] = size(X);
 % Fit model to each boostrap sample of data
 for m = 1:nBootstraps
+%     fprintf('creating bootstrap X of n samples\n');
     %Create a bootstrap X of n samples
-    for i = 1:n
+    for i = 1:n/50
         j = ceil(n*rand()); % Pick a number from 1 to n
         Xbst(i,:) = X(j,:);
         ybst(i,1) = y(j);
     end
+%       fprintf('fitting randomtree\n');
       model.subModel{m} = randomTree(Xbst,ybst,depth); %for 2.2.4
 end
 
