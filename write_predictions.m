a8304041dc62635793efaf37b9415155a6aa3f8e
arivanbuuren <ariadna.estrada42@gmail.com>
2017-04-30 22:10:24 -0700

    New classification images

--- write_predictions.m
+++ write_predictions.m
@@ -1,8 +1,10 @@
 clear;
 % load('yhat.mat');
-% load('NeuralNet code/prediction.mat');
-load('predictionDT/yhat.mat')
-% yhat = ytest;
+% load('Data&Results_withoutIx/prediction_woIx.mat');
+% load('Data&Results_withoutIx/testingData.mat');
+% load('predictionDT/yhat.mat')
+load('Data&Results_withoutIx/predictionGMM.mat');
+yhat = yhat_test_GMM;
 im_w = 120;
 im_h = 160;
 numImages =  length(yhat) / (im_w * im_h) ; 
@@ -10,6 +12,6 @@ predictions = reshape(yhat, [im_w*im_h,numImages]);
 
 for im = 1:numImages
    yvis = visualizePrediction(predictions(:,im));
-   imName = char(strcat('classifiedImages/DT/image', string(im), '.png' ));
+   imName = char(strcat('classifiedImages/noIx/GMM/image', string(im), '.png' ));
    imwrite(yvis, imName);
 end
