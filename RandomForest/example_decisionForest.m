59f18ec63d5c680dc81d63722b7447900025a99d
arivanbuuren <ariadna.estrada42@gmail.com>
2017-05-01 15:51:49 -0700

    Running random forest on HP laptop

--- RandomForest/example_decisionForest.m
+++ RandomForest/example_decisionForest.m
@@ -4,18 +4,20 @@ clear
 clc
 load('../BestDataset/trainingData.mat');
 load('../BestDataset/testingData.mat');
-X = Xtrain;
+% X = Xtrain;
+X = Xtrain(:,1:3);
+Xtest = Xtest(:,1:3);
 y = ytrain;
 [n,d] = size(X);
 fprintf('running random forest\n');
 
 %Parameters from CV for with IxIy
-depth = 11;
-nBootstraps = 11;
+% depth = 11;
+% nBootstraps = 11;
 
 %Parameters from CV for w/o IxIy
-% depth = 6;
-% nBootstraps = 21;
+depth = 6;
+nBootstraps = 21;
 
 model = decisionForest(X,y,depth,nBootstraps);
 % Evaluate training error
@@ -39,4 +41,4 @@ fprintf('Average prediction time for one image: %.2f seconds\n', predTimeRF);
 
 yhat_test_RF = yhat;
 
-save('predictionRFwith.mat', 'yhat_train_RF', 'yhat_test_RF', 'predTimeRF');
+save('predictionRFwo.mat', 'yhat_train_RF', 'yhat_test_RF', 'predTimeRF');
