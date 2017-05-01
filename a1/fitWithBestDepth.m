a738bfd2110a589d9b979c819bf26b5d1547e98f
CandiceT93 <candicetian93@gmail.com>
2017-04-30 22:22:19 -0700

    results for DT without Ix

--- a1/fitWithBestDepth.m
+++ a1/fitWithBestDepth.m
@@ -1,9 +1,10 @@
-load 'trainingData.mat'
-load 'testingData.mat'
+load 'Data&Results_withoutIx/trainingData.mat'
+load 'Data&Results_withoutIx/testingData.mat'
 
 [n,d] = size(Xtrain);
 
-model = decisionTreeInfoGain(Xtrain, ytrain, 10);
+model = decisionTreeInfoGain(Xtrain, ytrain, 11);
+yhat_train_DT = model.predict(model, Xtrain);
 yhat = model.predict(model, Xtest);
 testAcc = mean(yhat == ytest);
 fprintf('Test accuracy is %.4f\n', testAcc);
