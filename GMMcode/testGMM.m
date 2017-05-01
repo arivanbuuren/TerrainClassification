f8954298964d6ecb65c1049d8d1dc444032e0619
arivanbuuren <ariadna.estrada42@gmail.com>
2017-05-01 14:59:32 -0700

    Some updates

--- GMMcode/testGMM.m
+++ GMMcode/testGMM.m
@@ -16,10 +16,16 @@ model = generativeGaussian(Xtrain, ytrain);
 fprintf('Gaussian Gen. Model. training accuracy is %.2f\n', mean(yhat_train_GMM == ytrain));
 
 % Make predictions on test data
+tStart = tic;
 [yhat_test_GMM, pyx_test_GMM] = model.predict(model, Xtest);
+tElapsed = toc(tStart);
 fprintf('Gaussian Gen. Model. testing accuracy is %.2f\n', mean(yhat_test_GMM == ytest));
+
+%Get average prediction time
+predTimeGMM =  tElapsed/(length(ytest)/(120*160));
+fprintf('Average prediction time for one image is: %.2f\n',predTimeGMM);
 
 %Save predictions and probabilities on training data
 pyx_train_GMM = pyx_train_GMM(:,1:7);
 pyx_test_GMM = pyx_test_GMM(:,1:7);
-save('predictionGMMwith.mat', 'yhat_train_GMM', 'pyx_train_GMM', 'yhat_test_GMM', 'pyx_test_GMM');
+save('predictionGMMwith.mat', 'yhat_train_GMM', 'pyx_train_GMM', 'yhat_test_GMM', 'pyx_test_GMM', 'predTimeGMM');
